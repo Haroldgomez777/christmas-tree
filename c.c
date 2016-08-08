504e243c0d26eed46ea19529a9af349604e8680e
Harold <haroldgomez40@gmail.com>
2016-08-08 20:13:10 +0530

    tiny c program

--- c.c
+++ c.c
@@ -3,31 +3,19 @@ int main()
 {
 int i,j,k,l,p;
 int c;
-
-
-
-
 for(i=0;i<5;i++)
 printf("\n");
-
 	for( i=10 ; i>0 ; i--)
-	{
-			
+	{	
 		for(j=0;j<i;j++)
 		{
 			printf(" ");
 		}
 		for(k=10;k>=i;k--)
 		{
-			
-			switch(k) {
-			  
+			switch(k) {  
 			 case 8:
-			 	if(i==4||i==6||i==1)
-			 		{
-			 			printf("$");
-			 			break;
-			 		}
+			 	if(i==4||i==6||i==1){printf("$");break;}
 			 	else
 			  	printf("*");
 			  	break;
@@ -42,35 +30,10 @@ printf("\n");
 			}			
 		
 		}
-		for(l=10;l>i;l--)
-				{	if(i==1&&l==3)
-					{
-						printf("$");		
-					}
+		for(l=10;l>i;l--){if(i==1&&l==3)
+					{printf("$");}
 					else
-					{
-						printf("*");
-					}	
-				}
-		printf("\n");
-	}
-		
-	
-
-
-for(l=0;l<2;l++)
-	{
-		for(p=0;p<9;p++)
-		{
-			printf(" ");
-		}
-		printf("***\n");
-	}
-
-
-
-
-printf("Merry Christmas\n");
-
-}
-
+					{printf("*");}}printf("\n");}for(l=0;l<2;l++)
+	{for(p=0;p<9;p++)
+		{printf(" ");}printf("***\n");
+	}printf("Merry Christmas\n");}
